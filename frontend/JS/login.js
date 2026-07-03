31ad66f70ca8ddeb78b8302d1ca403393b71b90a
tainandogomes-coder <tainando_gomes@estudante.sesisenai.org.br>
2026-07-02 21:14:48 -0300

    comita

---
+++ frontend/JS/login.js
@@ -0,0 +1,6 @@
+const botaoEntrar = document.getElementById("botaoEntrar");
+
+botaoEntrar.addEventListener("click", (e) => {
+    e.preventDefault();
+     window.location.href = "../pages/home.html";
+});
