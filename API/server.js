f8b3a038b68bebb0777237846a1077e37d621b39
DuduGriebeler <raul.griebeler26@gmail.com>
2026-07-09 20:23:00 -0300

    AtualizaçãoServer

--- API/server.js
+++ API/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.port || 3031;
 const cors = require("cors")
 
-const pacienteRoute = require("./routes/pacienteRoute");
+const pacienteRoute = require("./routes/pacientesRoute");
 const agendamentosRoute = require("./routes/agendamentosRoutes");
 const medicosRoute = require("./routes/medicosRoutes");
 
